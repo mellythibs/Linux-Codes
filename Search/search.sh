8d01e71b5a7c7a2af47b108cb1afa25618e4c6aa
mellythibs <37309694+mellythibs@users.noreply.github.com>
2021-02-11 02:22:36 -0500

    Update search.sh

--- Search/search.sh
+++ Search/search.sh
@@ -3,10 +3,11 @@
 echo 'Term searching tool... same thing as the cltr+f shortcut'
 echo 'Which code takes in two files, a .txt file of terms and the input file'
 echo ' '
-
+#here
 
 while IFS= read -r inputTerm 
 do
-	egrep 'inputTerm' inputFile.txt
+	egrep "$inputTerm" inputFile.txt   #Curr: outputs full line rather than just the term itself. 
 
 done < terms.txt
+
