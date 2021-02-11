c331570c9b0b398df5c84dfdd13be806646c6892
mellythibs <37309694+mellythibs@users.noreply.github.com>
2021-02-11 02:15:23 -0500

    Update search.sh

--- Search/search.sh
+++ Search/search.sh
@@ -1,1 +1,12 @@
+#2/11/2021
 
+echo 'Term searching tool... same thing as the cltr+f shortcut'
+echo 'Which code takes in two files, a .txt file of terms and the input file'
+echo ' '
+
+
+while IFS= read -r inputTerm 
+do
+	egrep 'inputTerm' inputFile.txt
+
+done < terms.txt
